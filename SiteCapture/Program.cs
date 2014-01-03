a1d09717ccde4254ecd42881a3f19ea60e1c274f
Amged Rustom <amgadhs@codehill.com>
2014-01-03 18:53:25 +0400

    Disabled IE's clicking sound when opening pages.

--- SiteCapture/Program.cs
+++ SiteCapture/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,6 +9,43 @@ namespace SiteCapture
 {
     static class Program
     {
+        #region Disable IE's clicking sound when rendering a page
+        /* For more info:
+         * http://social.msdn.microsoft.com/Forums/vstudio/en-US/1940f513-8b72-4a29-9a57-85427d8fa88a/how-to-disable-click-sound-in-webbrowser-control?forum=csharpgeneral
+         * http://stackoverflow.com/questions/10456/howto-disable-webbrowser-click-sound-in-your-app-only
+         * http://stackoverflow.com/questions/393166/how-to-disable-click-sound-in-webbrowser-control
+         * http://www.hackcommunity.com/Thread-VB-Net-C-Disable-Sound-of-the-WebBrowser-Control
+         * http://stackoverflow.com/questions/1720902/how-to-disable-click-sound-using-a-webbrowser-element-in-c-sharp
+         * http://stackoverflow.com/questions/10629499/muting-refresh-sound-in-webbrowser-control-c-sharp-winform
+         */
+
+        const int FEATURE_DISABLE_NAVIGATION_SOUNDS = 21;
+        const int SET_FEATURE_ON_THREAD = 0x00000001;
+        const int SET_FEATURE_ON_PROCESS = 0x00000002;
+        const int SET_FEATURE_IN_REGISTRY = 0x00000004;
+        const int SET_FEATURE_ON_THREAD_LOCALMACHINE = 0x00000008;
+        const int SET_FEATURE_ON_THREAD_INTRANET = 0x00000010;
+        const int SET_FEATURE_ON_THREAD_TRUSTED = 0x00000020;
+        const int SET_FEATURE_ON_THREAD_INTERNET = 0x00000040;
+        const int SET_FEATURE_ON_THREAD_RESTRICTED = 0x00000080;
+
+        [DllImport("urlmon.dll")]
+        [PreserveSig]
+        [return: MarshalAs(UnmanagedType.Error)]
+        static extern int CoInternetSetFeatureEnabled(
+            int FeatureEntry,
+            [MarshalAs(UnmanagedType.U4)] int dwFlags,
+            bool fEnable);
+
+        static void DisableClickSounds()
+        {
+            CoInternetSetFeatureEnabled(
+                FEATURE_DISABLE_NAVIGATION_SOUNDS,
+                SET_FEATURE_ON_PROCESS,
+                true);
+        }
+        #endregion
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -21,6 +59,8 @@ namespace SiteCapture
             //if there is another instance running exit
             if (!result)
                 return;
+
+            DisableClickSounds();
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
